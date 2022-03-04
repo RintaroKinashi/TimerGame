2e7e64508766b0b953812058e19cfc81bf5ff74e
RintaroKinashi <nagisa624174@gmail,com>
2022-03-04 16:13:44 +0900

    [update]refs #1_Completion_stopwatch

--- js/main.js
+++ js/main.js
@@ -5,4 +5,57 @@
   const start = document.getElementById('start');
   const stop = document.getElementById('stop');
   const reset = document.getElementById('reset');
+
+  let startTime;
+  let timeoutId;
+
+  // 関数
+
+  function countUp() {
+    const elapsedTime = new Date(Date.now() - startTime);
+    const elapsedSeconds = String(elapsedTime.getSeconds()).padStart(2, '0');
+    const elapsedMilliSeconds = String(elapsedTime.getMilliseconds()).padStart(3, '0');
+
+    timer.textContent = `${elapsedSeconds}.${elapsedMilliSeconds}`;
+
+    timeoutId = setTimeout(() => {countUp();}, 10);
+  }
+
+  function setButtonStateInitial() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+  }
+
+  function setButtonStateRunning() {
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+  }
+
+  function setButtonStateStopped() {
+    start.disabled = true;
+    stop.disabled = true;
+    reset.disabled = false;
+  }
+
+  // イベント
+
+  setButtonStateInitial();
+
+  start.addEventListener('click', () => {
+    setButtonStateRunning();
+    startTime = Date.now();
+    countUp();
+  });
+
+  stop.addEventListener('click', () => {
+    setButtonStateStopped();
+    clearTimeout(timeoutId);
+  });
+
+  reset.addEventListener('click', () => {
+    setButtonStateInitial();
+    timer.textContent = '00.000';
+  });
 }
