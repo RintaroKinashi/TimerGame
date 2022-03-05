d0864316cbb48039739a66a11177073520f6e139
RintaroKinashi <nagisa624174@gmail,com>
2022-03-05 15:20:28 +0900

    [add]composer.json

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
+<?php
+echo 'Hello world';
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1">
   <title>TimerGame!</title>
   <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
   <div class="container">
     <div id="message">
@@ -26,4 +32,5 @@
     <p>©2022 <a href="https://twitter.com/1119rinritu">@1119rinritu</a></p>
   </ul>
 </footer>
+
 </html>
