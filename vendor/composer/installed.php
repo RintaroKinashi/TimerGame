52c4adbe3435834da7c35bb9666f1f03b90dc7ca
RintaroKinashi <nagisa624174@gmail,com>
2022-03-18 21:01:07 +0900

    [add]refs #12_Prepare_for_Deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f7eb316cb4aca903a803362142d038a1241419a8',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f7eb316cb4aca903a803362142d038a1241419a8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
